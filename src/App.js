49533945f406f5ce966f8e2b79b6ebcf408216fd
Joko Sarjono S <johno.smakaduta@gmail.com>
2018-10-28 23:50:12 +0700

    Counter Sederhana

--- src/App.js
+++ src/App.js
@@ -1,48 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import HeaderClass from './components/Header'
+import Counter from './components/Counter'
 import './App.css';
 
 class App extends Component {
-  state ={
-    nama:'Popeye'
-  };
+ 
   render() {
     return (
-      // <div className="App">
-      //   <header className="App-header">
-      //     <img src={logo} className="App-logo" alt="logo" />
-      //     <h1 className="App-title">Selamat Datang, Joko Sarjono Slameto</h1>
-      //   </header>
-      //   <p className="App-intro">
-      //     To get started, edit <code>src/App.js</code> and save to reload.
-      //   </p>
-      // </div>
-      
-      <div>
-      <HeaderClass nama={this.state.nama} kelas="10" />
-      <HeaderClass nama="Agung" kelas="11" />
-      <HeaderClass nama="Rudi" kelas="12" />
-      <HeaderClass nama="Adi" kelas="13" />
-      <HeaderClass nama="Rahmat" kelas="14" />
-      </div>
+     <div>
+       <Counter name="Lari"/>
+       <Counter name="Makan"/>
+       <Counter name="Minum"/>
+       <Counter name="Tidur"/>
+     </div>
     );
   }
 }
-// class Header extends Component{
-//   render(){
-//     return(
-//       <div>
-//         <h1>Ini Header</h1>
-//       </div>
-//     )
-//   }
-// }
-// const Header =()=>{
-//   return(
-//     <h1>Ini Header Functional</h1>
-//   )
-// }
+
 
 
 export default App;
