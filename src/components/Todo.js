614795c84611302558b55891342c1767b5f859ec
Joko Sarjono S <johno.smakaduta@gmail.com>
2018-11-24 23:47:19 +0700

    Layouting React Strap dan CSS

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,111 +1,112 @@
-import React, {Component} from "react";
+import React, { Component } from 'react';
 import { Button, Input } from 'reactstrap';
 import './todo.css';
 
-class Todo extends Component{
-    state ={
-        database : [],
-        input: "",
-        buttonView : false,
-        id : 0
-    }
+class Todo extends Component {
+	state = {
+		database: [],
+		input: '',
+		buttonView: false,
+		id: 0
+	};
 
-handleChange =e=>{
-    this.setState({
-        input : e.target.value
-    });
-    console.log(this.state.input)
-};
+	handleChange = (e) => {
+		this.setState({
+			input: e.target.value
+		});
+		console.log(this.state.input);
+	};
 
-addData=()=>{
-    const database = this.state.database;
-    const dataInput = this.state.input;
-    database.push(dataInput);
-    localStorage.setItem("todos", JSON.stringify(database));
-    // console.log(database);
-    this.setState({
-        input:""
-    });
-};
+	addData = () => {
+		const database = this.state.database;
+		const dataInput = this.state.input;
+		database.push(dataInput);
+		localStorage.setItem('todos', JSON.stringify(database));
+		// console.log(database);
+		this.setState({
+			input: ''
+		});
+	};
 
-getData=()=>{
-    let getItem =JSON.parse(localStorage.getItem('todos'));
-    if(getItem !== null){
-    return this.setState({
-        database : getItem
-    });
+	getData = () => {
+		let getItem = JSON.parse(localStorage.getItem('todos'));
+		if (getItem !== null) {
+			return this.setState({
+				database: getItem
+			});
+		}
+		console.log(getItem);
+	};
+
+	getDataId = (index) => {
+		const database = this.state.database;
+		const getData = database[index];
+		this.setState({
+			input: getData,
+			buttonView: true,
+			id: index
+		});
+	};
+
+	editData = (id) => {
+		const database = this.state.database;
+		database.splice(id, 1, this.state.input);
+		localStorage.setItem('todos', JSON.stringify(database));
+		this.getData();
+		this.setState({
+			input: '',
+			buttonView: false
+		});
+	};
+
+	buttonCancel = () => {
+		this.setState({
+			buttonView: false,
+			input: ''
+		});
+	};
+
+	deleteData = (index) => {
+		const database = this.state.database;
+		database.splice(index, 1);
+		localStorage.setItem('todos', JSON.stringify(database));
+		// console.log(database);
+		this.getData();
+	};
+	componentDidMount() {
+		this.getData();
+	}
+
+	render() {
+		return (
+			<div className="backgroundtodo">
+				<h1 align="center">Todo</h1>
+				<Input type="text" onChange={this.handleChange} value={this.state.input} />
+				<br />
+
+				{this.state.buttonView ? (
+					<div>
+						<Button onClick={() => this.editData(this.state.id)}>Edit</Button>
+						<Button onClick={() => this.buttonCancel()}>Batal</Button>
+					</div>
+				) : (
+					<Button onClick={() => this.addData()}>Simpan</Button>
+				)}
+
+				<ul>
+					{this.state.database.map((datum, index) => {
+						return (
+							<div>
+								<li>{datum}</li>
+								<Button onClick={() => this.deleteData(index)}>Hapus</Button>
+								<Button onClick={() => this.getDataId(index)}>Edit</Button>
+							</div>
+						);
+					})}
+				</ul>
+			</div>
+		);
+	}
 }
-    console.log(getItem);
-};
 
-getDataId = (index)=>{
-    const database = this.state.database;
-    const getData = database[index]
-    this.setState({
-        input : getData,
-        buttonView : true,
-        id : index
-    });
-};
-
-editData = id => {
-    const database = this.state.database;
-    database.splice(id,1,this.state.input);
-    localStorage.setItem("todos", JSON.stringify(database));
-    this.getData();
-    this.setState({
-        input :"",
-        buttonView : false
-    })
-};
-
-buttonCancel =()=>{
-    this.setState({
-        buttonView:false,
-        input :""
-    })
-}
-
-
-deleteData= index =>{
-    const database = this.state.database;
-    database.splice(index,1);
-    localStorage.setItem("todos", JSON.stringify(database));
-    // console.log(database);
-    this.getData();
-};
-componentDidMount(){
-    this.getData();
-}
-
-render(){
-    return(
-        <div className="backgroundtodo">
-            <h1>Todo</h1>
-          <Input type="text" onChange={this.handleChange} value={this.state.input}/><br/>
-         
-          {this.state.buttonView ?(
-              <div>
-                   <button onClick={()=>this.editData(this.state.id)}>Edit</button>
-                  <button onClick={()=>this.buttonCancel()}>Batal</button>
-              </div>
-                 
-          ):( <Button onClick={()=>this.addData()}>Simpan</Button>)}
-      
-          <ul>
-           {this.state.database.map((datum,index) =>{
-               return(
-                   <div>
-                   <li>{datum}</li>
-                   <button onClick={()=>this.deleteData(index)}>Hapus</button>
-                   <button onClick={()=>this.getDataId(index)}>Edit</button>
-                   </div>
-               );
-           })}
-          </ul>
-        </div>
-    )
-}
-}
-
-export default Todo
+export default Todo;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css'; //import css
+import './components/todo.css';
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
 registerServiceWorker();
+
